94ea0106056d62a2c131dc5ed994b6b7c0ff81cd
ShinyaShimatani <agg.anwo.0914@gmail.com>
2021-05-01 04:13:22 +0900

    modify

--- src/app/Http/Controllers/MembersController.php
+++ src/app/Http/Controllers/MembersController.php
@@ -15,13 +15,13 @@ class MembersController extends Controller
         echo "データベースに接続できません";
         }*/
         $pdo = DB::connection()->getPdo();
-        $sql= "SELECT id, ’name', mimic, maso, pathos, updown, black, volume, energy, insane, color FROM members ORDER BY id";
+        $sql= "SELECT id, name, mimic, maso, pathos, updown, black, volume, energy, insane, color FROM members ORDER BY id";
         $stmt = $pdo->prepare($sql);
         $stmt->execute();
         $data= "";
         while($row = $stmt->fetchAll()){
         $data.= "{";
-        $data.= " label: '".$row['name']."',";
+        $data.= " label: '".$row["name"]."',";
         $data.= " data: [".$row["mimic"].", ".$row["maso"].", ".$row["mimic"].", ".$row["pathos"].", ";
         $data.= $row["updown"].", ".$row["black"].", ".$row["volume"].", ".$row["energy"].", ".$row["insane"]."],";
         $data.= " backgroundColor: 'rgba(".$row["color"].",0.4)',";
