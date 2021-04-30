257a6381b7528a7b81fba5fbd16ece2c9cd36fd0
ShinyaShimatani <agg.anwo.0914@gmail.com>
2021-04-30 20:51:03 +0900

    modify

--- src/resources/views/humor_chart.blade.php
+++ src/resources/views/humor_chart.blade.php
@@ -1,8 +1,10 @@
 <h1>graph</h1>
 ここにグラフを表示
+<table class="table table-striped">
 @foreach($members as $member)
         <tr>
             <td>{{$member->id}}</td>
             <td>{{$member->name}}</td>
         </tr>
 @endforeach
+</table>
