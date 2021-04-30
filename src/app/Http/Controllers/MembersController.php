cd2809033cd5cf4b5ce6b1646e79dd456fc3d026
ShinyaShimatani <agg.anwo.0914@gmail.com>
2021-05-01 02:31:49 +0900

    modify

--- src/app/Http/Controllers/MembersController.php
+++ src/app/Http/Controllers/MembersController.php
@@ -8,8 +8,18 @@ use App\Models\Humor;
 class MembersController extends Controller
 {
     public function show(Request $request){
-        $members = Humor::orderBy('id','asc')->get();
-    
-        return view('humor_chart', ['members' => $members]);
+    $members = DB::table('members');
+
+    // Generate random colours for the groups
+    for ($i=0; $i<=count(members); $i++) {
+           $colours[] = '#' . substr(str_shuffle('ABCDEF0123456789'), 0, 6);
+          }
+
+    // Prepare the data for returning with the view
+    $chart = new Chart;
+      $chart->labels = (array_keys($members));
+      $chart->dataset = (array_values($members));
+      $chart->colours = $colours;
+    return view('humor_chart', compact('chart'));
     }
 }
--- src/resources/views/humor_chart.blade.php
+++ src/resources/views/humor_chart.blade.php
@@ -11,53 +11,33 @@
 	<!-- グラフを描画 -->
    <script>
 	//ラベル
-	var labels = [
-		"mimic",
-		"maso",
-		"pathos",
-		"updown",
-		"black",
-		"volume",
-        "energy",
-        "insane",
-	];
+	var labels : [
+    'モノマネ',
+    '自虐',
+    '哀愁',
+    '緩急',
+    'ブラックジョーク',
+    '声量',
+    '熱量'
+    '狂気'
+  ],
 
 	//グラフを描画
    var ctx = document.getElementById("myChart");
    var myChart = new Chart(ctx, {
 		type: 'radar',
 		data : {
-			labels: labels,
+			labels: {!!json_encode($chart->labels)!!},
 			datasets: [{
-             label: 'Sheldon',
-             data: [65, 59, 90, 81, 56, 55, 40, 100],
+             label: {{ $members->id }},
+             data: {!! json_encode($chart->dataset)!!},
              fill: true,
-             backgroundColor: 'rgba(255, 99, 132, 0.2)',
+             backgroundColor: {!! json_encode($chart->colours)!!},
              borderColor: 'rgb(255, 99, 132)',
              pointBackgroundColor: 'rgb(255, 99, 132)',
              pointBorderColor: '#fff',
              pointHoverBackgroundColor: '#fff',
              pointHoverBorderColor: 'rgb(255, 99, 132)'
-            }, {
-             label: 'Penny',
-             data: [50, 78, 10, 89, 30, 85, 60, 90],
-             fill: true,
-             backgroundColor: 'rgba(54, 162, 235, 0.2)',
-             borderColor: 'rgb(54, 162, 235)',
-             pointBackgroundColor: 'rgb(54, 162, 235)',
-             pointBorderColor: '#fff',
-             pointHoverBackgroundColor: '#fff',
-             pointHoverBorderColor: 'rgb(54, 162, 235)'
-            }, {
-             label: 'Mike',
-             data: [28, 48, 40, 19, 96, 27, 100, 50],
-             fill: true,
-             backgroundColor: 'rgba(70, 70, 70, 0.3)',
-             borderColor: 'rgb(90, 102, 555)',
-             pointBackgroundColor: 'rgb(54, 162, 235)',
-             pointBorderColor: '#fff',
-             pointHoverBackgroundColor: '#fff',
-             pointHoverBorderColor: 'rgb(54, 162, 235)'
             }]
 		},
 		options: {
