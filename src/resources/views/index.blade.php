6aad88b6491bd6a77174c1020a8ee1ec702d0f14
ShinyaShimatani <agg.anwo.0914@gmail.com>
2021-05-01 19:36:50 +0900

    modify

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -11,14 +11,14 @@
  <div class="form-group, text-left" > 
  <p1><strong>AND検索</strong></p1>
  <form action="{{url('/search')}}" method="get" class="text-left">
-     <p2>モノマネ: <input type="number" name="mimic" value="mimic" style="width:80px;"> 以上</p2><br>
-     <p2>自虐: <input type="number" name="maso" value="maso" style="width:80px;"> 以上</p2><br>
-     <p2>哀愁: <input type="number" name="pathos" value="pathos" style="width:80px;"> 以上</p2><br>
-     <p2>緩急: <input type="number" name="updown" value="updown" style="width:80px;"> 以上</p2><br>
-     <p2>ブラックジョーク: <input type="number" name="black_joking" value="black_joking" style="width:80px;"> 以上</p2><br>
-     <p2>声量: <input type="number" name="volume" value="volume" style="width:80px;"> 以上</p2><br>
-     <p2>熱量: <input type="number" name="energy" value="energy" style="width:80px;"> 以上</p2><br>
-     <p2>狂気: <input type="number" name="insane" value="insane" style="width:80px;"> 以上</p2><br>
+     <p2>　ﾓﾉﾏﾈ : <input type="number" name="mimic" value="mimic" style="width:80px;"> 以上</p2><br>
+     <p2>　自虐 : <input type="number" name="maso" value="maso" style="width:80px;"> 以上</p2><br>
+     <p2>　哀愁 : <input type="number" name="pathos" value="pathos" style="width:80px;"> 以上</p2><br>
+     <p2>　緩急 : <input type="number" name="updown" value="updown" style="width:80px;"> 以上</p2><br>
+     <p2>ﾌﾞﾗｯｸｼﾞｮｰｸ: <input type="number" name="black_joking" value="black_joking" style="width:80px;"> 以上</p2><br>
+     <p2>　声量 : <input type="number" name="volume" value="volume" style="width:80px;"> 以上</p2><br>
+     <p2>　熱量 : <input type="number" name="energy" value="energy" style="width:80px;"> 以上</p2><br>
+     <p2>　狂気 : <input type="number" name="insane" value="insane" style="width:80px;"> 以上</p2><br>
      <p2><input type="submit" value="検索"></p2>
  </form>
  <a href="{{ route('index') }}">検索解除</a>
