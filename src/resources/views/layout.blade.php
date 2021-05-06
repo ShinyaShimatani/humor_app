26907df5477df67e12b8cdf6814cab5d04b2a1cf
ShinyaShimatani <agg.anwo.0914@gmail.com>
2021-05-06 21:03:57 +0900

    modify

--- src/resources/views/layout.blade.php
+++ src/resources/views/layout.blade.php
@@ -9,19 +9,6 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
-        <link href="css/bootstrap.min.css" rel="stylesheet"><!--　←　触らないBootstrap v4.3.1　-->
-        <script src="js/jquery-3.4.1.min.js"></script><!--　←　触らない　-->
-        <script src="js/bootstrap.min.js"></script><!--　←　触らない　-->
-        <script src="js/Chart.min.js"></script>  
-
-        <style>
-        h1{font-size:1.3rem;}
-        .table-font { font-size:0.7rem; }
-        @media screen and (max-width: 480px) { 
-        	.sm-non { display:none; }
-        } 
-        </style>
-
         <link rel="stylesheet" 
               href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
               integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" 
