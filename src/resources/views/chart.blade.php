5e26db5d7411b4d84ad645895576940f72a16f34
ShinyaShimatani <agg.anwo.0914@gmail.com>
2021-05-06 21:08:50 +0900

    modify

--- src/resources/views/chart.blade.php
+++ src/resources/views/chart.blade.php
@@ -3,49 +3,82 @@
 @section('content')
  
 <h1>Humor-User-Index</h1>
+<h4>ユーモアチャート</h4>
+   	<canvas id="myChart"></canvas>
+		<script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js"></script>
+	<!-- グラフを描画 -->
+   <script>
+	//ラベル
+	var labels = [
+		"2020年1月",
+		"2020年2月",
+		"2020年3月",
+		"2020年4月",
+		"2020年5月",
+		"2020年6月",
+	];
+	//平均体重ログ
+	var average_weight_log = [
+		50.0,	//1月のデータ
+		51.0,	//2月のデータ
+		52.0,	//3月のデータ
+		53.0,	//4月のデータ
+		54.0,	//5月のデータ
+		49.0	//6月のデータ
+	];
+	//最大体重ログ
+	var max_weight_log = [
+		52.0,	//1月のデータ
+		54.0,	//2月のデータ
+		55.0,	//3月のデータ
+		51.0,	//4月のデータ
+		57.0,	//5月のデータ
+		48.0	//6月のデータ
+	];
+	//最小体重ログ
+	var min_weight_log = [
+		48.0,	//1月のデータ
+		47.0,	//2月のデータ
+		45.0,	//3月のデータ
+		44.0,	//4月のデータ
+		43.0,	//5月のデータ
+		49.0	//6月のデータ
+	];
 
- <title>レーダーチャート</title>
-   
-    
-
-<div class="container">	
-    <h1 class="text-primary my-3">レーダーチャート </h1>
-	<div class="row">
-		<div class="col-md-7">	
-            <!--以下グラフ-->
-            <canvas id="myChart"></canvas>
-            <script>
-            var ctx = document.getElementById('myChart').getContext('2d');
-            var myChart = new Chart(ctx, {
-                type: 'radar',
-                data: {
-                    labels: ['モノマネ', '自虐', '哀愁', '緩急', 'ブラックジョーク', '声量', '熱量', '狂気'],
-                    datasets: [{
-                     label: 'Penny',
-                     data: [100,100,100,100,100,100,100,100],
-                     fill: true,
-                     backgroundColor: 'rgba(255, 99, 132, 0.2)',
-                     borderColor: 'rgb(255, 99, 132)',
-                     pointBackgroundColor: 'rgb(255, 99, 132)',
-                     pointBorderColor: '#fff',
-                     pointHoverBackgroundColor: '#fff',
-                     pointHoverBorderColor: 'rgb(255, 99, 132)'
-                    }, {
-                     label: 'Sheldon',
-                     data: [10,10,10,10,10,100,10,10],
-                     fill: true,
-                     backgroundColor: 'rgba(54, 162, 235, 0.2)',
-                     borderColor: 'rgb(54, 162, 235)',
-                     pointBackgroundColor: 'rgb(54, 162, 235)',
-                     pointBorderColor: '#fff',
-                     pointHoverBackgroundColor: '#fff',
-                     pointHoverBorderColor: 'rgb(54, 162, 235)'
-                    }]}
-                }
-            );
-            </script>
-            <!--以上グラフ-->
-		</div>
-	</div>
+	//グラフを描画
+   var ctx = document.getElementById("myChart");
+   var myChart = new Chart(ctx, {
+		type: 'line',
+		data : {
+			labels: labels,
+			datasets: [
+				{
+					label: '平均体重',
+					data: average_weight_log,
+					borderColor: "rgba(0,0,255,1)",
+         			backgroundColor: "rgba(0,0,0,0)"
+				},
+				{
+					label: '最大',
+					data: max_weight_log,
+					borderColor: "rgba(0,255,0,1)",
+         			backgroundColor: "rgba(0,0,0,0)"
+				},
+				{
+					label: '最小',
+					data: min_weight_log,
+					borderColor: "rgba(255,0,0,1)",
+         			backgroundColor: "rgba(0,0,0,0)"
+				}
+			]
+		},
+		options: {
+			title: {
+				display: true,
+				text: '体重ログ（６ヶ月平均）'
+			}
+		}
+   });
+   </script>
 
 @endsection
